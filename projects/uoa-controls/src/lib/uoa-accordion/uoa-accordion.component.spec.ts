f63277cb5294bfb87823bf61fd4f0f1ab788e5db
Ryan <ryan.c@auckland.ac.nz>
2020-02-17 16:40:17 +1300

    accordion: multiSelect, firstPanelStartsOpen

--- projects/uoa-controls/src/lib/uoa-accordion/uoa-accordion.component.spec.ts
+++ projects/uoa-controls/src/lib/uoa-accordion/uoa-accordion.component.spec.ts
@@ -6,26 +6,42 @@ import { UoaAccordionComponent } from './uoa-accordion.component';
 import { UoaExpandablePanelComponent } from '../uoa-expandable-panel/uoa-expandable-panel.component';
 
 @Component({
-    selector: 'uoa-accordion-with-mock-data',
+    selector: 'uoa-accordion-with-mock-data-positive',
     template: `
+      <uoa-accordion firstPanelStartsOpen="true" multiSelect="true">
+          <uoa-expandable-panel>Mock data</uoa-expandable-panel>
+          <uoa-expandable-panel>Different mock data</uoa-expandable-panel>
+      </uoa-accordion>
+    `
+})
+
+class MockUoaAccordionWrapperPositive {
+    @ViewChild(UoaAccordionComponent, {static: true}) accordion: UoaAccordionComponent;
+}
+@Component({
+  selector: 'uoa-accordion-with-mock-negative',
+  template: `
     <uoa-accordion>
         <uoa-expandable-panel>Mock data</uoa-expandable-panel>
         <uoa-expandable-panel>Different mock data</uoa-expandable-panel>
     </uoa-accordion>
-    `
+  `
 })
-class MockUoaAccordionWrapper {
-    @ViewChild(UoaAccordionComponent, {static: true}) accordion: UoaAccordionComponent;
+class MockUoaAccordionWrapperNegative {
+  @ViewChild(UoaAccordionComponent, {static: true}) accordion: UoaAccordionComponent;
 }
 
-describe('UoaAccordionComponent', () => {
-  let component: UoaAccordionComponent;
-  let fixture: ComponentFixture<MockUoaAccordionWrapper>;
+/*
+Positive cases
+*/
+describe('UoaAccordionComponent: firstPanelStartsOpen, multiSelect', () => {
+  let accordion: UoaAccordionComponent;
+  let fixture: ComponentFixture<MockUoaAccordionWrapperPositive>;
   
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-            MockUoaAccordionWrapper,
+            MockUoaAccordionWrapperPositive,
             UoaAccordionComponent,
             UoaExpandablePanelComponent
         ],
@@ -37,19 +53,72 @@ describe('UoaAccordionComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MockUoaAccordionWrapper);
-    component = fixture.componentInstance.accordion;
+    fixture = TestBed.createComponent(MockUoaAccordionWrapperPositive);
+    accordion = fixture.componentInstance.accordion;
     fixture.detectChanges();
   });
 
   it('should exist when created in another component', () => {
-    expect(component).toBeTruthy();
+    expect(accordion).toBeTruthy();
   });
 
   it('opens the first panel on init', () => {
-    component.ngAfterContentInit();
-    expect(component.panels.toArray()[0].opened).toBeTruthy();
-    expect(component.panels.toArray()[1].opened).toBeFalsy();
+    accordion.ngAfterContentInit();
+    expect(accordion.panels.toArray()[0].opened).toBeTruthy();
+    expect(accordion.panels.toArray()[1].opened).toBeFalsy();
+  });
+  
+  it('toggles both panels when clicked', () => {
+    let headers = fixture.debugElement.nativeElement.querySelectorAll('.panel-heading');
+    headers[0].click();
+    headers[1].click();
+    
+    expect(accordion.panels.toArray()[0].opened).toBeFalsy(); // remember we also set this one to start open
+    expect(accordion.panels.toArray()[1].opened).toBeTruthy();
   });
   
 });
+
+/*
+Negative cases
+*/
+describe('UoaAccordionComponent: default', () => {
+  let accordion: UoaAccordionComponent;
+  let fixture: ComponentFixture<MockUoaAccordionWrapperNegative>;
+  
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+            MockUoaAccordionWrapperNegative,
+            UoaAccordionComponent,
+            UoaExpandablePanelComponent
+        ],
+        imports: [
+            NoopAnimationsModule
+        ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MockUoaAccordionWrapperNegative);
+    accordion = fixture.componentInstance.accordion;
+    fixture.detectChanges();
+  });
+
+  it('does not open the first panel on init', () => {
+    accordion.ngAfterContentInit();
+    expect(accordion.panels.toArray()[0].opened).toBeFalsy();
+    expect(accordion.panels.toArray()[1].opened).toBeFalsy();
+  });
+  
+  it('leaves just one panel open after two were clicked', () => {
+    let headers = fixture.debugElement.nativeElement.querySelectorAll('.panel-heading');
+    headers[0].click();
+    headers[1].click();
+    expect(accordion.panels.toArray()[0].opened).toBeFalsy();
+    expect(accordion.panels.toArray()[1].opened).toBeTruthy();
+  });
+  
+});
+
--- projects/uoa-controls/src/lib/uoa-accordion/uoa-accordion.component.ts
+++ projects/uoa-controls/src/lib/uoa-accordion/uoa-accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { Component, ContentChildren, QueryList, AfterContentInit, Input, Output, EventEmitter } from '@angular/core';
 import { Expandable } from '../interfaces/expandable';
 import { UoaExpandablePanelComponent } from '../uoa-expandable-panel/uoa-expandable-panel.component';
 
@@ -11,24 +11,32 @@ export class UoaAccordionComponent  implements AfterContentInit {
   // waiting for this issue to be resolved in order to use more generic types
   // https://github.com/angular/angular/issues/8563
   @ContentChildren(UoaExpandablePanelComponent) panels: QueryList<Expandable>;
- 
+  @Input() firstPanelStartsOpen: boolean = false;
+  @Input() multiSelect: boolean = false;
+  @Output() expanded = new EventEmitter<Expandable>();
+
   ngAfterContentInit() {
     // Open the first panel
-    this.panels.toArray()[0].opened = true;
+    if (this.firstPanelStartsOpen) {
+      this.panels.toArray()[0].opened = true;
+    }
     // Loop through all panels
     this.panels.toArray().forEach((panel: Expandable) => {
       // subscribe panel toggle event
       panel.toggle.subscribe(() => {
         // Open the panel
         this.openPanel(panel);
+        this.expanded.emit(panel);
       });
     });
   }
  
   openPanel(panel: Expandable) {
     // close all panels
-    this.panels.toArray().forEach(p => p.opened = false);
-    // open the selected panel
-    panel.opened = true;
+    if (!this.multiSelect) {
+      this.panels.toArray().forEach(p => p.opened = false);
+    }
+    // toggle the selected panel
+    panel.opened = !panel.opened;
   }
 }
